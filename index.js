1a7c248fa153a6a2e7982866de48e4eceadbe9a0
Mikhail Fedorov <464d41@gmail.com>
2021-03-19 17:45:52 -0700

    export header class

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const Policy = require("./lib/policy");
+const Policy = require("./lib/policy").Policy;
+const Header = require("./lib/policy").Header;
 const isValidPolicy = require("./lib/schemas/policy")
 
-module.exports = { Policy, isValidPolicy };
+module.exports = { Policy, Header, isValidPolicy };
