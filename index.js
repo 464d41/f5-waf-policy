31a5d25224f7610df65ec7a1e7620e2d8fca6cd6
Mikhail Fedorov <464d41@gmail.com>
2021-03-30 15:59:30 -0700

    export new classes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 "use strict";
 
 const Policy = require("./lib/policy").Policy;
-const Header = require("./lib/policy").Header;
+const Method = require("./lib/policy").Method;
 const Url = require("./lib/policy").Url;
+const Filetype = require("./lib/policy").Filetype;
+const Header = require("./lib/policy").Header;
+const Parameter = require("./lib/policy").Parameter;
+
 const isValidPolicy = require("./lib/schemas/policy")
 
-module.exports = { Policy, Header, Url, isValidPolicy };
+module.exports = { Policy, Method, Url, Filetype, Header, Parameter, isValidPolicy };
